0847ab74a3a9680c01da6236c675880b3189ddf7
Kevin Walker <k.walker@sae-sf.net>
2022-03-07 23:53:37 +0100

    Move files, update hover events, fix acces

--- pages/web-design/index.js
+++ pages/web-design/index.js
@@ -3,8 +3,11 @@ const navToggle = document.querySelector('.mobile-nav-btn')
 const overlay = document.querySelector('.overlay')
 // const heroContainer = document.querySelector('.hero-container')
 const mainSection = document.querySelector('main')
+const secondaryGrid = document.querySelector('.secondary-grid')
+
 
 function handleMenuToggle(e) {
+    const blockTab = document.querySelector('.block-tab')
     const windowWidth = window.innerWidth;
     const isButton = e.target.localName === 'button'
     const isVisible = primaryNav.getAttribute('data-visible') === 'true'
@@ -15,7 +18,10 @@ function handleMenuToggle(e) {
         overlay.setAttribute('data-visible', true)
         // heroContainer.setAttribute('data-visible', false)
 
+        secondaryGrid.style.zIndex = -3
         document.body.style.overflow = 'hidden'
+
+        blockTab.addEventListener('keydown', handleTabEvent)
         
     }
     else if (isVisible || windowWidth >= 740) {
@@ -24,11 +30,23 @@ function handleMenuToggle(e) {
         overlay.setAttribute('data-visible', false)
         // heroContainer.setAttribute('data-visible', true)
 
+        secondaryGrid.style.zIndex = 0
         document.body.style.overflow = ''
+
+        blockTab.removeEventListener('keydown', handleTabEvent)
+    }
+}
+
+function handleTabEvent(e) {
+    let nextPos = document.querySelector('.start')
+    console.log(e.keyCode)
+    if (e.keyCode == 9) {
+        nextPos.focus()
     }
 }
 
 navToggle.addEventListener('click', handleMenuToggle)
 mainSection.addEventListener('click', handleMenuToggle)
 
-window.addEventListener('resize', handleMenuToggle) 
+window.addEventListener('resize', handleMenuToggle)
+
