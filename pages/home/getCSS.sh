f67414a8e87dec3388a07c85548370560bd3194a
Kevin Walker <k.walker@sae-sf.net>
2022-03-18 18:25:13 +0100

    Organize files and resolve conflicts

--- pages/home/getCSS.sh
+++ pages/home/getCSS.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-echo "/* CSS Buile */" > ../testDeleteMe/test.css
+rm ./home.css
 
-names=$(find /home/kwalker/STEM/Dev/Projects/designo/designo-multi-page-website/designo/pages/home | grep '.css$')
+names=$(find $PWD | grep '.css$')
     
 SAVEIFS=$IFS   # Save current IFS (Internal Field Separator)
 IFS=$'\n'      # Change IFS to newline char
 names=($names) # split the `names` string into an array by the same name
 IFS=$SAVEIFS   # Restore original IFS
 
+echo "/* CSS Buile */" > ./home.css
+
 for (( i=0; i<${#names[@]}; i++ ))
 do
-    cat "${names[$i]}" >> ../testDeleteMe/test.css
+    cat "${names[$i]}" >> ./home.css
 done
