496ed0155f4cbcf7fce7383206d3b476417e9bab
Kevin Walker <k.walker@sae-sf.net>
2022-04-07 10:48:12 -0500

    Fix invalid html markup

--- pages/contact/form.js
+++ pages/contact/form.js
@@ -2,26 +2,56 @@
 let input = {
     name: "",
     email: "",
-    message: ""
+    msg: ""
 };
 
 let form = document.querySelector(".form");
 
-form.addEventListener("submit", (e) => {
-    for (props in input) {
-	if(input[props] == "") {
-	    let attr = document.querySelector(`.field_${props}`);
-	    e.preventDefault();
+function isValidEmail(email) {
+    let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return regex.test(email);
+}
 
-	    attr.setAttribute("data-has-error", true);
+function isEmptyField(value) {
+    return value === "";
+}
+
+function setAttribute(name) {
+    let el = document.querySelector(`.field_${name}`);
+    el.setAttribute("data-has-error", true);   
+}
+
+function showEmptyWarning(name) {
+    let elText = document.querySelector(`.error__text_${name}`);
+    elText.textContent = "Can't be empty";
+}
+
+function showEmailWarning(email) {
+    let invalidEmailText = document.querySelector(".error__text_email");
+    invalidEmailText.textContent = "Please enter a valid email";
+   
+}
+
+form.addEventListener("submit", (e) => {
+    for (name in input) {
+	
+	if(isEmptyField(input[name])) {
+	    setAttribute(name);
+	    showEmptyWarning(name);
+	    e.preventDefault();
+	}
+	else if (name == "email" && !isValidEmail(input[name])) {
+	    setAttribute(name);
+	    showEmailWarning(name);
+	    e.preventDefault();
 	}
     }
 });
 
 form.addEventListener("input", (e) => {
     let { value, name } = e.target;
-    let attr = document.querySelector(`.field_${name}`);
     input[name] = value;
 
-    attr.setAttribute("data-has-error", false);
+    let el = document.querySelector(`.field_${name}`);
+    el.setAttribute("data-has-error", false);
 });
